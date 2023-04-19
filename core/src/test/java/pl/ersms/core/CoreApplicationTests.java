30269c8c45426b38001128451e84df59ca8f36df
Łukasz Błocki <lukasz.blocki.stud@pw.edu.pl>
2023-04-19 21:06:52 +0200

    Initial commit

---
+++ core/src/test/java/pl/ersms/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ersms.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

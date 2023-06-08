6446c779a5765425ca6a607267bcdd7e31f10936
Michal <czesio007p@gmail.com>
2023-06-08 20:27:45 +0200

    added restaurant list

---
+++ frontend/src/app/restaurant-list/restaurant-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-list',
+  templateUrl: './restaurant-list.component.html',
+  styleUrls: ['./restaurant-list.component.scss']
+})
+export class RestaurantListComponent {
+  restaurants = [
+    {
+      id: 1,
+      name: "pensjonat Bekas",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus tortor eu dolor tempus venenatis. Proin commodo justo augue. Integer tempor, dolor tempus ullamcorper interdum, lorem",
+      open: "4:20 - 21:37",
+      lowestPrice: "30zł",
+      highestPrice: "60zł",
+      count: 15
+    }
+  ]
+
+}

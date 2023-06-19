fe4855c4f3f8874224c3ec4e572c2e32013a32cf
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-06-19 22:39:06 +0200

    Add email sending TODO

--- notifications/Ersms/Ersms.Notifications/Program.cs
+++ notifications/Ersms/Ersms.Notifications/Program.cs
@@ -1,3 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues?tabs=connection-string#receive-messages-from-the-queue
+using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+// the client that owns the connection and can be used to create senders and receivers
+ServiceBusClient client;
+
+// the processor that reads and processes messages from the queue
+ServiceBusProcessor processor;
+
+// The Service Bus client types are safe to cache and use as a singleton for the lifetime
+// of the application, which is best practice when messages are being published or read
+// regularly.
+//
+// Set the transport type to AmqpWebSockets so that the ServiceBusClient uses port 443. 
+// If you use the default AmqpTcp, make sure that ports 5671 and 5672 are open.
+
+var serviceBusConnectionString = "<FILL>";
+var queueName = "notifications";
+var emailServiceConnectionString = "<FILL>";
+
+var clientOptions = new ServiceBusClientOptions()
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+client = new ServiceBusClient(serviceBusConnectionString, clientOptions);
+
+// create a processor that we can use to process the messages
+processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    // add handler to process messages
+    processor.ProcessMessageAsync += MessageHandler;
+
+    // add handler to process any errors
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    // start processing 
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    // stop processing 
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    // Calling DisposeAsync on client types is required to ensure that network
+    // resources and other unmanaged objects are properly cleaned up.
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // complete the message. message is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/email/send-email?tabs=windows%2Cconnection-string&pivots=programming-language-csharp
+async Task SendEmail(string recipient)
+{
+    var sender = "<FILL>";
+    var subject = "Zamówienie";
+    var content = """
+    <html>
+        <body>
+            <h1>Hi there!</h1>
+            <p>This is a test email from the Azure Service Bus queue.</p>
+        </body>
+    </html>
+    """;
+    // TODO: send email
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
